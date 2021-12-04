aaea1dc7cbdeb04e3c34bac2377a8bf9bd71269f
PoYang Chen <poyang31@yahoo.com.tw>
2021-12-04 14:04:13 +0000

    init

---
+++ 前端/assets/js/startPage.js
@@ -0,0 +1,7 @@
+export default function startPage() {
+  const sp = `
+        <button id="employee">員工資料</button>
+        <div id="content"></div>
+    `;
+  return sp;
+}
---
+++ 後端/vendor/Controller.php
@@ -0,0 +1,9 @@
+<?php
+abstract class Controller{
+    protected static function response($status, $message, $result=NULL){
+        $resp['status'] = $status;
+        $resp['message'] = $message;
+        $resp['result'] = $result;
+        return $resp;
+    }
+}
